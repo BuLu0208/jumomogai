a024230c0227a0bc668f33eb50bb20fe7d1a8fe7
BuLu0208 <BuLu0208@users.noreply.github.com>
2026-04-27 01:01:50 +0800

    fix: use #include instead of #import for C headers

--- TrollHelper/TSHRootViewController.m
+++ TrollHelper/TSHRootViewController.m
@@ -1,8 +1,8 @@
 #import "TSHRootViewController.h"
 #import <TSUtil.h>
 #import <TSPresentationDelegate.h>
-#import <dlfcn.h>
-#import <sys/utsname.h>
+#include <dlfcn.h>
+#include <sys/utsname.h>
 
 // ========== 卡密验证系统 ==========
 #define KAMI_API_URL @"https://kami.lengye.top"
